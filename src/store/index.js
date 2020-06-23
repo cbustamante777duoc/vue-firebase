f131e4b2582494522c706408075e285b916364c4
cristian <cra.bustamante@alumnos.duoc.cl>
2020-06-22 22:45:37 -0400

    v-for de componente de inicio

--- src/store/index.js
+++ src/store/index.js
@@ -6,20 +6,30 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    tareas:[]
   },
   mutations: {
+    setTareas(state,payload){
+      state.tareas = payload
+    }
   },
   actions: {
     getTareas({commit}){
+      const tareas = []
       //funcion que recibe el nombre de la coleccion 
       db.collection('tareas').get()
       // si la respuesta es buena
         .then(res =>{
           // recorre la colecion de objetos
           res.forEach(item =>{
-            console.log(`la id es ${item.id}`)
-            console.log(item.data())
+            /*console.log(`la id es ${item.id}`)
+            console.log(item.data())*/
+            let tarea = item.data()
+            tarea.id = item.id
+            tareas.push(tarea)
           })
+          //metodos que van a hacer ocupados en mutaciones
+          commit('setTareas',tareas)
 
         })
 
--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -1,24 +1,30 @@
 <template>
     <div>
        <h1>componente de inicio</h1> 
+       <ul>
+         <li v-for="(item, index) in tareas" :key="index">
+             {{item.nombre}} - {{item.id}}
+
+         </li>
+        </ul>
     </div>
+    
 </template>
 
 <script>
-import {mapActions} from 'vuex'
+import {mapActions, mapState} from 'vuex'
 export default {
     name:'Inicio',
-   /* created() {
+   created() {
         // uso del metodo
         this.getTareas()
-        
-    },*/
-    mounted() {
-         this.getTareas()
         
     },
     methods: {
         ...mapActions(['getTareas'])
+    },
+    computed: {
+        ...mapState(['tareas'])
     },
 }
 </script>
